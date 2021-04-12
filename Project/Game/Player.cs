f8f5311f64217a5167350f30e65c3e106c53bfd7
WeakPlayer-rgb <kkishun00@gmail.com>
2021-04-12 14:04:34 +0500

    TwoMethods

--- Project/Game/Player.cs
+++ Project/Game/Player.cs
@@ -2,15 +2,9 @@
 {
     public class Player : IGameObject
     {
-        public string GetImage(string path)
-        {
-            throw new System.NotImplementedException();
-        }
+        public string GetImage(string path) => "car.png";
 
-        public int DrawPrioritet(int priority)
-        {
-            throw new System.NotImplementedException();
-        }
+        public int DrawPrioritet(int priority) => 0;
 
         public void Move()
         {
