450c1a59320253928abaff9ed82cae4c2fbaa47b
GizIOn <34921969+GizIOn@users.noreply.github.com>
2021-04-12 21:46:39 +0500

    Added some stuff

---
+++ Project/Game/Physics.cs
@@ -0,0 +1,29 @@
+﻿namespace Game
+{
+    public class Physics
+    {
+        private readonly double mass;
+        private readonly double maxTurnRate;
+
+        public Physics() : this(1.0, 0.25)
+        {
+        }
+
+        public Physics(double mass, double maxTurnRate)
+        {
+            this.mass = mass;
+            this.maxTurnRate = maxTurnRate;
+        }
+
+        public Car MoveRocket(Car car, double forceValue, Turn turn, double dt)
+        {
+            var turnRate = turn == Turn.Left ? -maxTurnRate : turn == Turn.Right ? maxTurnRate : 0;
+            var dir = car.Direction + turnRate * dt;
+            var force = new Vector(forceValue, 0).Rotate(car.Direction);
+            var velocity = car.Velocity + force * dt / mass;
+            var location = car.Location + velocity * dt;
+            velocity = velocity * (1 - 0.01 * dt);
+            return new Car(location, velocity, dir, car.Time + 1, car.TakenCheckpointsCount);
+        }
+    }
+}
---
+++ Project/Game/Turn.cs
@@ -0,0 +1,9 @@
+﻿namespace Game
+{
+    public enum Turn
+    {
+        None = 0,
+        Left = -1,
+        Right = 1
+    }
+}
